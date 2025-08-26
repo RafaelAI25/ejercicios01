ac51bb52dc4988e0898b771c62567278a5b5668f
RafaelAI25 <rafael.docencia23@gmail.com>
2025-08-26 08:52:19 +0100

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Suma de 2 números*/
+        Scanner sc = new Scanner(System.in);
+
+        // Pedimos los datos al usuario
+        System.out.println("introduce el primer número entero: ");
+        int a = sc.nextInt();
+
+        System.out.println("Introduce el segundo número entero: ");
+        int b = sc.nextInt();
+
+        int suma = a + b;
+
+        System.out.println("La suma es: " + suma);
+
+
+        // Area del rectángulo
+        System.out.println("Vamos a calcular el área del rectángulo");
+        System.out.println("Introduce la base del rectángulo");
+        int base = sc.nextInt();
+        System.out.println("Introduce la altura del rectángulo");
+        int altura = sc.nextInt();
+
+
+        // Celcius a Fahrenheit
+
+    }
+}
